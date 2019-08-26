022f8e8f60bd7cb4ec4d8e4fdb51751d8b0a9bdd
Michael McCullough <michael.mccullough21@gmail.com>
2019-08-26 15:35:24 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,9 +15,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species)
+  "SELECT species, COUNT(species)
   FROM characters
-  ORDER BY species DESC LIMIT 1"
+  GROUP BY species
+  ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
